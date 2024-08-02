ffdd9f723cf6ec5ca576ea36f12f668d896ef3ab
suneeb <ssuneebvishnu@gmail.com>
2024-08-03 00:29:00 +0530

    deletion implemented

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -57,31 +57,81 @@ class linkedList:
         new_node=Node(data)
         new_node.ref=temp.ref
         temp.ref=new_node 
+    
+    def del_beg(self):
+        if self.head is None:
+            print("Linked list is empty")
+        else:
+            self.head=self.head.ref
+
+    def del_end(self):
+        if self.head is None:
+            print("Linked list is empty")
+        elif self.head.ref is None:
+            self.head=None
+        else:
+            n=self.head
+            while n.ref.ref is not None:
+                n=n.ref
+            n.ref=None
+
+    def del_val(self):
+        if self.head is None:
+            print("List is empty")
+            return
+        x=int(input("Enter the value to be deleted: "))
+        if x == self.head.data:
+            self.head=self.head.ref
+            return
+        n=self.head
+        while n.ref is not None:
+            if x == n.ref.data:
+                break
+            n=n.ref
+        if n.ref is None:
+            print("Element not found")
+        else:
+            n.ref=n.ref.ref
+    
+    def delete(self):
+        if self.head is None:
+            print("List is empty")
+            return
+        c=int(input("\nWhere do you want to perform deletion:\n1.At beginning\n2.At end\n3.delete a specific value\n"))
+        if c == 1:
+            self.del_beg()
+        elif c == 2:
+             self.del_end()
+        elif c == 3:
+            self.del_val()
+        else:
+            print("Wrong choice!")
+
+    def insert(self):
+        ch=int(input("\nWhat operation? \n1.Insert at beginnig\n2.Insert at end\n3.insert at position\n4.display\n"))
+        if ch ==1:
+            num=int(input("Enter the number: "))
+            ll.add_begin(num)
+        elif ch ==2:
+            num=int(input("Enter the number: "))
+            ll.add_end(num)
+        elif ch ==3:
+            num=int(input("Enter the number: "))
+            ll.add_pos(num)
+        else:
+            print("Wrong choice!")
 
 
 
 
 ll=linkedList()
 while True:
-    ch=int(input("\nWhat operation? \n1.Insert at beginnig\n2.Insert at end\n3.insert at position\n4.display\n"))
+    ch=int(input("\nWhat operation? \n1.Insertion\n2.Deletion\n3.Display\n4.Exit\n"))
     if ch ==1:
-        num=int(input("Enter the number: "))
-        ll.add_begin(num)
+        ll.insert()
     if ch ==2:
-        num=int(input("Enter the number: "))
-        ll.add_end(num)
+        ll.delete()
     if ch ==3:
-        num=int(input("Enter the number: "))
-        ll.add_pos(num)
-    if ch==4:
         ll.print_LL()
-    if ch==5:
+    if ch==4:
         break
-
-
-
-# ll.add_begin(20)
-# ll.add_end(30)
-# ll.add_pos(40)
-# ll.print_LL()
-
