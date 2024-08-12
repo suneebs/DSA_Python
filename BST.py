1bac28a9978aa087e869a06500cebd62e388e02c
suneeb <ssuneebvishnu@gmail.com>
2024-08-13 00:37:04 +0530

    Insertion Implemented

--- BST.py
+++ BST.py
@@ -4,8 +4,25 @@ class BST():
         self.lchild = None
         self.rchild = None
 
+    def insert(self,data):
+        if self.key == None:
+            self.key = data
+            return
+        if self.key == data:
+            return
+        if self.key > data:
+            if self.lchild:
+                self.lchild.insert(data)
+            else:
+                self.lchild = BST(data)
+        else:
+            if self.rchild:
+                self.rchild.insert(data)
+            else: 
+                self.rchild  = BST(data)
 root = BST(10)
 root.lchild = BST(5)
+root.insert(20)
 
 print(root.key)
 print(root.lchild)
