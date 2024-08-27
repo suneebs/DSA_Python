5955816773591c81f7e5c237d965d04860223659
suneeb <ssuneebvishnu@gmail.com>
2024-08-27 10:40:12 +0530

    Adding edge

--- Graph.py
+++ Graph.py
@@ -15,6 +15,18 @@ def add_node(v):
             temp.append(0)
         graph.append(temp)
 
+def add_edge(v1,v2,cost):
+    if v1 not in nodes:
+        print(v1,"is not present in the graph")
+    elif v2 not in nodes:
+        print(v2,"is not present in the graph")
+    else:
+        index1 = nodes.index(v1)
+        index2 = nodes.index(v2)  
+        # 1 for unweighted, cost for weighted
+        graph[index1][index2] =  cost  
+        # graph[index2][index1] =  cost   [ for directed graph ]
+   
 def print_graph():
     print(nodes)
     for i in range(node_count):
@@ -31,5 +43,7 @@ print_graph()
 add_node("A")
 add_node("B")
 add_node("C")
+add_edge("A","B",10)
+add_edge("C","B",5)
 print("After insertion: ")
 print_graph()
